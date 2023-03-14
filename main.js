ca42705d515a5113e2d65083580e52e4dc7fcd8f
felipePiani <111539507+felipePiani@users.noreply.github.com>
2023-03-14 18:45:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//ok
+//deslike
+//acenando
+//https://teachablemachine.withgoogle.com/models/82SbDKccg/
+
+
+//var prediction_1 = ""
+//var prediction_2 = ""
+
+//Webcam.set({
+//    width:350,
+//    height:300,
+//    image_format : 'png',
+//    png_quality:90
+//})
